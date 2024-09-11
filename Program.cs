f0846a4fd65bb47195999ce9b8febed561d3abb8
Vanduc250824 <leduc250824@gmail.com>
2024-09-11 10:05:23 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BaiTap;
+public class BaiTap()
+{
+    public static void Main(string[] args)
+    {
+        OPP p = new OPP();
+        p.Bai1();
+        p.Bai2();
+        p.Bai3();
+        p.Bai4();
+        p.Bai5();
+        p.Bai6();
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+![image](https://github.com/user-attachments/assets/9df6bc33-26f9-4e81-8956-3c8aab68a518)
